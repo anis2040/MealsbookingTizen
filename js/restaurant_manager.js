209d6faec2848d7c1a3ba3145a5d0dc49355bebd
chakib <chakib.fathallah@esprit.tn>
2019-11-19 12:11:27 +0100

    Consuming WS topten

--- js/restaurant_manager.js
+++ js/restaurant_manager.js
@@ -1,7 +1,6 @@
-/*
+
 window.onload = function() {
     // TODO:: Do your initialization job
-
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if (e.keyName === "back") {
@@ -11,10 +10,10 @@ window.onload = function() {
         }
     });
     
-    restaurants();  test 
+    restaurants();   
     
 };
-*/
+
 function getRestaurantById(idrestaurant){
 		
 		localStorage.setItem('id_restaurant', idrestaurant);
@@ -24,27 +23,32 @@ function getRestaurantById(idrestaurant){
 
 function restaurants(){
 	
-	var url = 'http://127.0.0.1:8000/api/restaurants';
+	var url = 'http://172.20.10.4:8000/api/restaurants';
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
+	
 	xhr.onload = function() {
 		var i=0;
 		var row="";
 		 // var userid = localStorage.getItem("userid");
-
+		
 	  if (this.status == 200 ) {
 		  
 		  var restaurant = JSON.parse(this.responseText);
-		  for (i = 0; i <restaurant.missions.length; i++) { 
+		 // console.log(restaurant[1].name); // JSON response
+		 
+		  for (i = 0; i <restaurant.length; i++) { 
+			    row = row + '<div class="card "> <div class="card-body"> <div class="media"> <a href="#" class="square-40 mr-3"><img src="'+restaurant[i].photo+'" alt=""></a> <a href="#" class="media-body"> <h5>'+restaurant[i].name+'</h5> <p>'+restaurant[i].category+'</p> </a> <a class="like-heart color-red"> <i class="icon material-icons">favorite</i> </a> </div> </div>       <div class="card-footer pt-0"> <div class="row"> <div class="col-auto"> <i class="material-icons text-warning">star</i> <span class="post-seconds">4.9 <span>(254)</span></span> </div> <div class="col"> <i class="material-icons text-grey">monetization_on</i> <span class="post-seconds">'+restaurant[i].priceMin+' <span>$</span></span> </div> <div class="col"> <i class="material-icons text-grey">monetization_on</i> <span class="post-seconds">'+restaurant[i].priceMax+' <span>$$</span></span> </div> </div> </div></div>';  
+			 // row = row + ' <div class="card-body"> <div class="media"> <a href="#" class="square-40 mr-3"><img src="img/small1.jpg" alt=""></a> <a href="#" class="media-body"> <h5>'+restaurant[i].name+'</h5> <p>'+restaurant[i].category+'</p> </a> <a class="like-heart color-red"> <i class="icon material-icons">favorite</i> </a> </div>   </div> <div class="card-footer pt-0"> <div class="row"> <div class="col-auto"> <i class="material-icons text-warning">star</i> <span class="post-seconds">4.9 <span>(254)</span></span> </div> <div class="col"> <i class="material-icons text-grey">schedule</i> <span class="post-seconds">20 <span>min</span></span> </div> <div class="col"> <i class="material-icons text-grey">monetization_on</i> <span class="post-seconds">40 <span>$</span></span> </div> </div> </div> ';
+			  console.log('salem'+ restaurant.length);
+			  console.log(restaurant[i].name);
+
+		
 			  
-			
-			  row = row+'  <section class="container">  <section class="box special" ><h3><a href="detailsmission.html" onclick="getidmission('+mission.missions[i].id+')"><p>'+mission.missions[i].title+'</p><p>'+mission.missions[i].description+'</p><p>'+mission.missions[i].date+'</p><p>'+mission.missions[i].total+'</p><h3></section></section>';  
-			 
-		  
 		  }
 		  
 		document.getElementById("demo").innerHTML = row; 
-	    console.log('response', this.response); // JSON response  
+	   
 	  }
 	};
 xhr.send();
@@ -102,7 +106,7 @@ function detailRestaurant(){
 		        document.getElementById("total").innerHTML= total;
 			  
 			  
-	   // console.log('response', this.response); // JSON response  
+	   
 	  }
 	};
 xhr.send();
