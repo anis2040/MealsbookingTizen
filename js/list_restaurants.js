0240c8697cccb6fee64514c108d3d1cd25ed06cb
Anis Helaoui <halaoui@mobiblanc.com>
2019-11-20 10:05:00 +0100

    last changes

--- js/list_restaurants.js
+++ js/list_restaurants.js
@@ -9,7 +9,7 @@ function listRestaurants(){
 		  var boutiques = JSON.parse(this.responseText);
 //		  for (i = 0; i <boutiques.boutique.length; i++) { 
 //			row = row+'  <tr><td class="cart_product_img"><a href="#"><img src="http://192.168.79.1/fazet/boutiqueuploads/'+boutiques.boutique[i].nomboutique+'.jpg" alt="Product"></a></td><td class="cart_product_desc"><h5>'+boutiques.boutique[i].nomboutique+'</h5></td><td class="price"><span>'+boutiques.boutique[i].numtel+'</span></td><td><a href="Mesproduits.html" onclick="getidbout('+boutiques.boutique[i].idboutique+');"><h7>Voir details</h7></a></td></tr>';
-//                }
+//          }
 		document.getElementById("demomesboutique").innerHTML = row;  
 	    console.log('response', this.response); // JSON response  
 	  }
---
+++ js/rating.js
@@ -0,0 +1,77 @@
+function saveRatingToLS(rating){
+localStorage.setItem('rating', rating);
+}
+
+function saveReview() {
+	
+	   var badWords = ['shit','zah','bad'];
+	   var userId = 1;
+	   var restaurantId = localStorage.getItem("id_restaurant");
+	   var review = document.getElementById("review").value;
+	   var rating = localStorage.getItem("rating");
+	   
+	   var verify = localStorage.getItem("verify");
+	   console.log("verify",verify);
+	   console.log("if",verify == userId+restaurantId);
+	   for (i = 0; i <badWords.length; i++) {
+		   if (review.includes(badWords[i]) ){
+			   alert("you can't put bad words");
+			   return; 
+		   }
+	   }
+	   if (verify == userId+restaurantId) {
+		   alert("you can't review this restaurant more thank once");
+		   return;
+	   }
+	   else {
+		    var http = new XMLHttpRequest();
+			var url = 'http://127.0.0.1:8000/api/rating';
+			
+			var params = "user_id="+userId+"&restaurant_id="+restaurantId+"&review="+review+"&rating="+rating;
+			http.open('POST', url, true);
+
+			//Send the proper header information along with the request
+			http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			
+
+			http.onreadystatechange = function() {//Call a function when the state changes.
+			    if(http.readyState == 4 && http.status == 200) {
+			   
+			    }
+			}
+			 var storage = localStorage.setItem('verify', userId+restaurantId);
+		    console.log("storage",storage);
+			http.send(params);
+			location.reload();  
+			
+	   }
+	
+} ; 
+
+
+ 
+
+function ratings(){
+	
+	var url = 'http://127.0.0.1:8000/api/ratings';
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	
+	xhr.onload = function() {
+		var i=0;
+		var row = "";
+	  if (this.status == 200 ) {
+		  
+		  var rating = JSON.parse(this.responseText);
+		  for (i = 0; i <rating.length; i++) {
+			  row += ' <li class="list-group-item"> <a href="#" class="media"> <div class="w-auto h-100 mr-3"> <figure class="square-60"><img src="'+rating[i].user.avatar+'" alt=""></figure> </div> <div class="media-body"> <h5>'+rating[i].user.name+'</h5> <p>'+rating[i].rating+'</p> <p><span>'+rating[i].review+'</span></p> </div> </a> </li>'
+		  }
+		  
+	  //document.getElementById("resto").innerHTML = row;  
+		document.getElementById("rating").innerHTML = row;  
+
+	    
+	  }
+	};
+xhr.send();
+}
